51dbade817e8530df674a7ad5c8b1b105a647e7d
Shahad Alghamdi <52297631+shahadgh@users.noreply.github.com>
2025-11-05 00:38:20 +0300

    Insert SQL query for investor share percentage
    Added SQL query to calculate share percentage per investor.

--- answer.sql
+++ answer.sql
@@ -1,1 +1,15 @@
 -- ADD YOUR SQL QUERY HERE
+SELECT 
+  t1.investor_id,
+  sector.sector_name,
+  ROUND(t1.no_of_shares * 100 / total.total_shares, 2) AS share_percentage
+FROM investor_transactions t1
+JOIN 
+(
+SELECT investor_id, SUM(no_of_shares) AS total_shares
+  FROM investor_transactions
+  GROUP BY investor_id
+) AS total
+ON t1.investor_id = total.investor_id
+JOIN sectors sector
+ON t1.sector_id = sector.sector_id
